8b331dc9d4947c0c0817e01fab2cfd108eae3df4
HaydenRomick <hsromick1@gmail.com>
2024-12-02 14:42:29 -0500

    Initial Commit

---
+++ GreekConnect/GreekConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreekConnectApp.swift
+//  GreekConnect
+//
+//  Created by Hayden Romick on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GreekConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreekConnectTests/GreekConnectTests.swift
@@ -0,0 +1,16 @@
+//
+//  GreekConnectTests.swift
+//  GreekConnectTests
+//
+//  Created by Hayden Romick on 12/2/24.
+//
+
+import Testing
+
+struct GreekConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
